1ca71b7029c0c696a0921d61cc0ab49db7b5ebda
Hellty <korsovm@gmail.com>
2023-08-06 22:56:25 +0300

    feat: task brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pairs = new Map(bracketsConfig); 
+  let brackets = [];
+  for (const char of str) {
+    if(pairs.get(brackets.at(-1))===char) brackets.pop();
+    else brackets.push(char);
+  }
+  return brackets.length == 0
 }
